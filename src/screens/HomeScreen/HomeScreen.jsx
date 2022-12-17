5fb71f6687665183c9b16dbd92b86475f2054688
Kamil Augustak <kamil.augustak@outlook.com>
2022-12-17 05:43:30 +0100

    home init

--- src/screens/HomeScreen/HomeScreen.jsx
+++ src/screens/HomeScreen/HomeScreen.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import Background from "../../components/Background/Background";
 import {Image, KeyboardAvoidingView, Text, View} from "react-native";
-import style from "../AccountScreen/style_AccountScreen";
+import style from "../HomeScreen/style_HomeScreen";
 
 const HomeScreen = () => {
     return (
         <Background>
-            <View>
-                <Text>logo placeholder</Text>
-            </View>
-            <View style={style.tipBox}>
-                <View style={style.tipBoxLeft}>
-                    <Image source={require('../../../assets/megafon.png')} style={style.tipBoxImage}/>
+            <View style={style.container}>
+                <View>
+                    <Text>logo placeholder</Text>
                 </View>
-                <View style={style.tipBoxRight}>
-                    <Text style={style.tipBoxTitle}>Czy wiedziałeś że...</Text>
-                    <Text style={style.tipBoxText}>Tę aplikację zaprogramowało {"\n"} tylko 6 programistów w 24 godziny.</Text>
+                <View style={style.tipBox}>
+                    <View style={style.tipBoxLeft}>
+                        <Image source={require('../../../assets/megafon.png')} style={style.tipBoxImage}/>
+                    </View>
+                    <View style={style.tipBoxRight}>
+                        <Text style={style.tipBoxTitle}>Czy wiedziałeś że...</Text>
+                        <View style={{height: 3, backgroundColor: '#1877f2', width: 100}}/>
+                        <Text style={style.tipBoxText}>Tę aplikację zaprogramowało {"\n"} tylko 6 programistów w 24 godziny.</Text>
+                    </View>
                 </View>
             </View>
+
         </Background>
     );
 }
--- src/screens/HomeScreen/style_HomeScreen.js
+++ src/screens/HomeScreen/style_HomeScreen.js
@@ -7,19 +7,43 @@ export const localColors = {
 };
 
 export default StyleSheet.create({
+    container: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        width: screenWidth,
+        height: screenHeight,
+    },
     tipBox: {
-
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-around",
+        alignItems: "center",
+        width: screenWidth - 50,
+        height: 150,
+        backgroundColor: "#fff",
     },
     tipBoxRight: {
-
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-around",
+        alignItems: "flex-start",
     },
     tipBoxLeft: {
-
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-around",
+        borderRightColor: "#d9d9d9",
+        borderRightWidth: 1,
+        height: 110,
+        alignItems: "center",
+        padding: 10,
     },
     tipBoxText: {
-
+        fontSize: 16,
     },
     tipBoxTitle: {
-
+        fontSize: 24,
     }
 });
