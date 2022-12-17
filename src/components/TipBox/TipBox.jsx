56e33cf15fc3883fbe53dc4489ed0890054ee8f1
Bartłomiej Kudzia <bar.kud.11340@gmail.com>
2022-12-17 08:44:38 +0100

    CategoriesCarousel osobny component

--- src/components/TipBox/TipBox.jsx
+++ src/components/TipBox/TipBox.jsx
@@ -7,9 +7,9 @@ export default function TipBox({title, text}) {
         <View style={style.tipBox}>
             <Image source={require('../../../assets/megafon.png')} style={style.tipBoxImage} />
             <View style={style.tipBoxRight}>
-                <Text style={style.tipBoxTitle}>Czy wiedziałeś że...</Text>
+                <Text style={style.tipBoxTitle}>{title}</Text>
                 <View style={style.tipBoxLine}/>
-                <Text style={style.tipBoxText}>Krzysztof Mącznik ma dwa pesele i urodził się w samolocie bez obywatelstwa.</Text>
+                <Text style={style.tipBoxText}>{text}</Text>
             </View>
         </View>
     );
--- src/screens/HomeScreen/HomeScreen.jsx
+++ src/screens/HomeScreen/HomeScreen.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import Background from "../../components/Background/Background";
-import {View, Text} from "react-native";
+import {View, Text, ScrollView, TouchableNativeFeedback, TouchableWithoutFeedback} from "react-native";
 import style from "../HomeScreen/style_HomeScreen";
 import TipBox from "../../components/TipBox/TipBox";
+import CategoriesCarousel from "../../components/CategoriesCarousel/CategoriesCarousel";
 
 const HomeScreen = () => {
+    const categories = ["Papiesz", "Polag", "Jebau", "Mae", "Dziedzi"];
+
+    console.log(CategoriesCarousel.toString());
+
     return (
         <Background>
             <View style={style.container}>
                 <TipBox title="Czy wiedziałeś że..." text="Krzysztof Mącznik ma dwa pesele i urodził sięw samolocie bez obywatelstwa" />
+                <Text style={style.header}>Kategorie</Text>
+                <CategoriesCarousel categoryList={categories} />
             </View>
-            <Text style={style.header}>Kategorie</Text>
         </Background>
     );
 }
