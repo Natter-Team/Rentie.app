af280ad5d5812db611ef70b5458fca661d21cec3
mbeltowski <marcel.beltowski@gmail.com>
2022-12-17 03:55:21 +0100

    password & username set and validation

--- src/screens/AccountScreen/AccountScreen.jsx
+++ src/screens/AccountScreen/AccountScreen.jsx
@@ -1,4 +1,11 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  TextInput,
+  Button,
+  TouchableHighlight,
+} from "react-native";
 import React from "react";
 import Background from "../../components/Background/Background";
 import style, { localColors } from "./style_AccountScreen";
@@ -6,6 +13,15 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 import { local_colors } from "./style_AccountScreen";
 
 const AccountScreen = () => {
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+
+  const onPressHandler = (e) => {
+    if (password.lenght >= 8 && password.lenght <= 32) {
+      // if
+    }
+  };
+
   return (
     <Background>
       <View style={style.topBox}>
@@ -13,6 +29,58 @@ const AccountScreen = () => {
       </View>
       <View style={style.contentContainer}>
         <Text style={style.header}>Zaloguj się</Text>
+        <KeyboardAvoidingView
+          behavior="position"
+          keyboardVerticalOffset={250}
+          style={style.keyboardView}
+        >
+          {/* <Text>Nazwa uzytkownika</Text> */}
+          <Ionicons
+            name="ios-person-outline"
+            size={28}
+            style={{
+              top: 40,
+              zIndex: 1,
+              left: 8,
+              width: 28,
+            }}
+          />
+          <TextInput
+            style={style.textInput}
+            onChangeText={(e) => setUsername(e.target.value)}
+            placeholder="Podaj nazwę użytkownika"
+          ></TextInput>
+          {/* <Text>Hasło</Text> */}
+          <Ionicons
+            name="ios-lock-closed-outline"
+            size={28}
+            style={{
+              top: 40,
+              zIndex: 1,
+              left: 8,
+              width: 28,
+            }}
+          />
+          <TextInput
+            style={style.textInput}
+            placeholder="Podaj hasło"
+            onChangeText={(e) => setPassword(e.target.value)}
+            secureTextEntry={true}
+          />
+          <TouchableHighlight>
+            <Text
+              style={style.confirmButton}
+              onPress={(e) => onPressHandler(e)}
+            >
+              {" "}
+              Zaloguj się
+            </Text>
+          </TouchableHighlight>
+        </KeyboardAvoidingView>
+        <View style={style.horizontalLaneContainer}>
+          <View style={style.horizontalLane}></View>
+          <Text style={style.HrText}>Lub</Text>
+        </View>
         <View style={style.customLoginBox}>
           <Ionicons
             name="logo-facebook"
@@ -34,10 +102,6 @@ const AccountScreen = () => {
             }}
           />
           <Text>Kontynuuj z Apple</Text>
-        </View>
-        <View>
-          <View style={style.horizontalLane}></View>
-          <Text style={style.HrText}>Lub</Text>
         </View>
       </View>
     </Background>
--- src/screens/AccountScreen/style_AccountScreen.js
+++ src/screens/AccountScreen/style_AccountScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Dimensions } from "react-native";
+import { StyleSheet, Dimensions, TouchableWithoutFeedback } from "react-native";
 import { screenWidth, screenHeight } from "../../../assets/styles";
 
 export const localColors = {
@@ -8,7 +8,7 @@ export const localColors = {
 
 export default StyleSheet.create({
   topBox: {
-    height: 120,
+    height: 80,
     backgroundColor: "#fff",
     width: screenWidth,
     fontSize: 28,
@@ -29,17 +29,52 @@ export default StyleSheet.create({
     borderRadius: 6,
     padding: 10,
     width: screenWidth - 48,
+    marginBottom: 25,
   },
   horizontalLane: {
-    width: screenWidth,
+    marginTop: 15,
+    width: screenWidth - 48,
     height: StyleSheet.hairlineWidth,
-    backgroundColor: "#fff",
+    backgroundColor: "#000",
   },
   contentContainer: {
     display: "flex",
-    height: screenHeight - 120,
+    height: screenHeight - 120 - 40,
     flexDirection: "column",
     justifyContent: "flex-start",
     alignItems: "center",
+  },
+  horizontalLaneContainer: {
+    // position: "relative",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  HrText: {
+    top: -10,
+    backgroundColor: "#F2F2F3",
+    paddingHorizontal: 10,
+  },
+  textInput: {
+    width: screenWidth - 76,
+    height: 52,
+    borderRadius: 6,
+    backgroundColor: "#fff",
+    paddingLeft: 46,
+  },
+  keyboardView: {
+    // height: 250,
+    width: screenWidth - 76,
+  },
+  confirmButton: {
+    color: "white",
+    width: screenWidth - 76,
+    height: 32,
+    borderRadius: 12,
+    textAlign: "center",
+    backgroundColor: "#58ABE7",
+    marginTop: 16,
+    textAlignVertical: "middle",
+    fontSize: 20,
   },
 });
