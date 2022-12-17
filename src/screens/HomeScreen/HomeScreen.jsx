bbbecabac194ce07fb8484d8f69790e8228b908b
Bartłomiej Kudzia <bar.kud.11340@gmail.com>
2022-12-17 07:19:31 +0100

    move tipbox to separate component

--- src/screens/HomeScreen/HomeScreen.jsx
+++ src/screens/HomeScreen/HomeScreen.jsx
@@ -1,26 +1,14 @@
 import React from "react";
 import Background from "../../components/Background/Background";
-import {Image, KeyboardAvoidingView, Text, View} from "react-native";
+import {View, Text} from "react-native";
 import style from "../HomeScreen/style_HomeScreen";
+import TipBox from "../../components/TipBox/TipBox";
 
 const HomeScreen = () => {
     return (
         <Background>
             <View style={style.container}>
-                <View>
-                    <Text>logo placeholder</Text>
-                </View>
-                <View style={style.tipBox}>
-                    {/*<View style={style.tipBoxLeft}>
-                        <Image source={require('../../../assets/megafon.png')} style={style.tipBoxImage}/>
-                    </View>*/}
-                    <Image source={require('../../../assets/megafon.png')} style={style.tipBoxImage} />
-                    <View style={style.tipBoxRight}>
-                        <Text style={style.tipBoxTitle}>Czy wiedziałeś że...</Text>
-                        <View style={style.tipBoxLine}/>
-                        <Text style={style.tipBoxText}>Krzysztof Mącznik ma dwa pesele i urodził się w samolocie bez obywatelstwa.</Text>
-                    </View>
-                </View>
+                <TipBox title="Czy wiedziałeś że..." text="Krzysztof Mącznik ma dwa pesele i urodził sięw samolocie bez obywatelstwa" />
             </View>
 
         </Background>
